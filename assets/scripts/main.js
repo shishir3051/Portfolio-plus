7750b181afc6992a99b7ba866c66b1a239c83458
Gaziur Rahman Shishir <gaziur35-3051@diu.edu.bd>
2022-02-12 12:45:19 +0600

    sidebar fixed

---
+++ assets/scripts/main.js
@@ -0,0 +1,43 @@
+// go to top button
+let toTopbtn = document.getElementsByClassName("goTop")[0];
+let pin = document.getElementById("pin");
+let startingPoint = pin.offsetTop;
+
+function backTotop() {
+    if (window.pageYOffset <= startingPoint) {
+        toTopbtn.classList.add("goTop-hidden");
+        toTopbtn.classList.remove("goTop");
+    } else {
+        toTopbtn.classList.add("goTop");
+        toTopbtn.classList.remove("goTop-hidden");
+    }
+}
+
+// sticky bar
+const stickyBar = document.getElementById("stickyBar");
+let stickyPinTop = document.getElementsByClassName("stickyPin-top")[0];
+let positioning = stickyPinTop.offsetTop;
+let endPin = document.getElementById("end-pin");
+let endPositioning = endPin.offsetTop;
+const aboutMe = document.getElementById("about-me");
+
+function positionFixed() {
+    if (window.pageYOffset >= positioning) {
+        stickyBar.classList.add("halt");
+        aboutMe.style.marginLeft = "224px";
+    } else {
+        stickyBar.classList.remove("halt");
+        aboutMe.style.marginLeft = "auto";
+    }
+
+    if (window.pageYOffset >= endPositioning) {
+        stickyBar.classList.add("continue-aside");
+    } else {
+        stickyBar.classList.remove("continue-aside");
+    }
+}
+
+window.onscroll = function () {
+    positionFixed();
+    backTotop();
+};
